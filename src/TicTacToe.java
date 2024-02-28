cf1d06c78ffd73be7d95395f1c5021d37fdc76be
Pajarillojethr0 <jethropajarillo04@gmail.com>
2024-02-28 22:43:57 +0800

    New feature printing the 3x3 TicTacToe board added.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,4 +1,34 @@
-
 public class TicTacToe {
+
+	public TicTacToe() {
+		super();
+		
+		char[][] board = { { ' ', ' ', ' ' },
+				   { ' ', ' ', ' ' },
+				   { ' ', ' ', ' ' } };
+		
+		displayBoard(board);
+	}
+
+
+	private void displayBoard(char[][] board) {
+		int rowSize = board.length;
+		int colSize = board[0].length;
+		
+		for(int i = 0; i < rowSize; i++) {
+			for(int j = 0; j < colSize; j++) {
+				System.out.print(board[i][j]);
+				if(j != colSize-1) {
+					System.out.print("|");
+				}
+			}
+			System.out.println();
+			if(i != rowSize-1) {
+				System.out.println("-+-+-");
+			}
+		}
+		
+		System.out.print(" X turn");
+	}
 
 }
