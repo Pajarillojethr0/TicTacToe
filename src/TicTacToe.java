1697de6173c29473fc89410ac409f607d8bcb44e
Pajarillojethr0 <jethropajarillo04@gmail.com>
2024-02-29 20:29:20 +0800

    Added Computer Move

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -12,8 +13,65 @@ public class TicTacToe {
 		displayBoard(board);
 	}
 
+	public void getComputerMove(char[][]board) {
+		Random random = new Random();
+		int computerMove = random.nextInt(9) + 1;
+		System.out.println( "Computer move: " + computerMove);
+		
+		switch(computerMove) {
+	    case 1:
+	        if (board[0][0] == ' ') {
+	            board[0][0] = 'O';
+	        }
+	        break;
+	    case 2:
+	        if (board[0][1] == ' ') {
+	            board[0][1] = 'O';
+	        }
+	        break;
+	    case 3:
+	        if (board[0][2] == ' ') {
+	            board[0][2] = 'O';
+	        }
+	        break;
+	    case 4:
+	        if (board[1][0] == ' ') {
+	            board[1][0] = 'O';
+	        }
+	        break;
+	    case 5:
+	        if (board[1][1] == ' ') {
+	            board[1][1] = 'O';
+	        }
+	        break;
+	    case 6:
+	        if (board[1][2] == ' ') {
+	            board[1][2] = 'O';
+	        }
+	        break;
+	    case 7:
+	        if (board[2][0] == ' ') {
+	            board[2][0] = 'O';
+	        }
+	        break;
+	    case 8:
+	        if (board[2][1] == ' ') {
+	            board[2][1] = 'O';
+	        }
+	        break;
+	    case 9:
+	        if (board[2][2] == ' ') {
+	            board[2][2] = 'O';
+	        }
+	        break;
+	    default:
+	        System.out.println("Invalid Input");
+	        break;
+	    }
+		displayBoard(board);
+	}
 
-	private void getUserMove(char[][] board) {
+	public void getUserMove(char[][] board) {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter your move (1-9) : ");
 		String userInput = scan.nextLine();
@@ -50,12 +108,12 @@ public class TicTacToe {
 	        System.out.println("Invalid Input");
 	        break;
 	}
-
-
+		
+		getComputerMove(board);
 		displayBoard(board);
 	}
 	
-	private void displayBoard(char[][] board) {
+	public void displayBoard(char[][] board) {
 		int rowSize = board.length;
 		int colSize = board[0].length;
 		
@@ -71,9 +129,8 @@ public class TicTacToe {
 				System.out.println("-+-+-");
 			}
 		}
-		
+//		getComputerMove(board);
 		getUserMove(board);
-		
 	}
 
 }
